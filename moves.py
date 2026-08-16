85cacba4fe590e76087fc50c6b36fd9425192e9e
kushagrbhatia0315-dot <kushagrbhatia0315@gmail.com>
2026-08-16 14:58:31 +0530

    Update moves.py

--- moves.py
+++ moves.py
@@ -1,71 +1,72 @@
 
 class move:
-    def __init__(self,name,base_type,accuracy,power,physical):
+    def __init__(self,name,base_type,accuracy,power,physical,status):
         self.name=name
         self.power=power
         self.base_type=base_type
         self.accuracy=accuracy
         self.physical=physical
-admin=move("admin", "normal",100,2000,True)
+admin=move("admin", "normal",100,2000,True,False)
 # --- NORMAL TYPE (10) ---
-tackle = move("tackle", "normal", 100, 40, True)
-scratch = move("scratch", "normal", 100, 40, True)
-pound = move("pound", "normal", 100, 40, True)
-quick_attack = move("quick_attack", "normal", 100, 40, True)
-headbutt = move("headbutt", "normal", 100, 70, True)
-slam = move("slam", "normal", 75, 80, True)
-body_slam = move("body_slam", "normal", 100, 85, True)
-take_down = move("take_down", "normal", 85, 90, True)
-double_edge = move("double_edge", "normal", 100, 120, True)
-hyper_beam = move("hyper_beam", "normal", 90, 150, False)
+tackle = move("tackle", "normal", 100, 40, True,False)
+scratch = move("scratch", "normal", 100, 40, True,False)
+pound = move("pound", "normal", 100, 40, True,False)
+quick_attack = move("quick_attack", "normal", 100, 40, True,False)
+headbutt = move("headbutt", "normal", 100, 70, True,False)
+slam = move("slam", "normal", 75, 80, True,False)
+body_slam = move("body_slam", "normal", 100, 85, True,False)
+take_down = move("take_down", "normal", 85, 90, True,False)
+double_edge = move("double_edge", "normal", 100, 120, True,False)
+hyper_beam = move("hyper_beam", "normal", 90, 150, False,False)
 
 # --- FIRE TYPE (10) ---
-ember = move("ember", "fire", 100, 40, False)
-fire_spin = move("fire_spin", "fire", 85, 35, False)
-flame_wheel = move("flame_wheel", "fire", 100, 60, True)
-fire_fang = move("fire_fang", "fire", 95, 65, True)
-fire_punch = move("fire_punch", "fire", 100, 75, True)
-lava_plume = move("lava_plume", "fire", 100, 80, False)
-flamethrower = move("flamethrower", "fire", 100, 90, False)
-heat_wave = move("heat_wave", "fire", 90, 95, False)
-fire_blast = move("fire_blast", "fire", 85, 110, False)
-flare_blitz = move("flare_blitz", "fire", 100, 120, True)
+ember = move("ember", "fire", 100, 40, False,False)
+fire_spin = move("fire_spin", "fire", 85, 35, False,False)
+flame_wheel = move("flame_wheel", "fire", 100, 60, True,False)
+fire_fang = move("fire_fang", "fire", 95, 65, True,False)
+fire_punch = move("fire_punch", "fire", 100, 75, True,False)
+lava_plume = move("lava_plume", "fire", 100, 80, False,False)
+flamethrower = move("flamethrower", "fire", 100, 90, False,False)
+heat_wave = move("heat_wave", "fire", 90, 95, False,False)
+fire_blast = move("fire_blast", "fire", 85, 110, False,False)
+flare_blitz = move("flare_blitz", "fire", 100, 120, True,False)
 
-# --- WATER TYPE (10) ---
-water_gun = move("water_gun", "water", 100, 40, False)
-bubble = move("bubble", "water", 100, 40, False)
-bubble_beam = move("bubble_beam", "water", 100, 65, False)
-water_pulse = move("water_pulse", "water", 100, 60, False)
-waterfall = move("waterfall", "water", 100, 80, True)
-dive = move("dive", "water", 100, 80, True)
-surf = move("surf", "water", 100, 90, False)
-aqua_tail = move("aqua_tail", "water", 90, 90, True)
-muddy_water = move("muddy_water", "water", 85, 90, False)
-hydro_pump = move("hydro_pump", "water", 80, 110, False)
+# --- WATER TYPE 
+water_gun = move("water_gun", "water", 100, 40, False,False)
+bubble = move("bubble", "water", 100, 40, False,False)
+bubble_beam = move("bubble_beam", "water", 100, 65, False,False)
+water_pulse = move("water_pulse", "water", 100, 60, False,False)
+waterfall = move("waterfall", "water", 100, 80, True,False)
+dive = move("dive", "water", 100, 80, True,False)
+surf = move("surf", "water", 100, 90, False,False)
+aqua_tail = move("aqua_tail", "water", 90, 90, True,False)
+muddy_water = move("muddy_water", "water", 85, 90, False,False)
+hydro_pump = move("hydro_pump", "water", 80, 110, False,False)
 
-# --- GRASS TYPE (10) ---
-absorb = move("absorb", "grass", 100, 20, False)
-mega_drain = move("mega_drain", "grass", 100, 40, False)
-vine_whip = move("vine_whip", "grass", 100, 45, True)
-razor_leaf = move("razor_leaf", "grass", 95, 55, True)
-magical_leaf = move("magical_leaf", "grass", 100, 60, False)
-giga_drain = move("giga_drain", "grass", 100, 75, False)
-seed_bomb = move("seed_bomb", "grass", 100, 80, True)
-energy_ball = move("energy_ball", "grass", 100, 90, False)
-leaf_blade = move("leaf_blade", "grass", 100, 90, True)
-solar_beam = move("solar_beam", "grass", 100, 120, False)
+# --- GRASS TYPE 
+absorb = move("absorb", "grass", 100, 20, False,False)
+mega_drain = move("mega_drain", "grass", 100, 40, False,False)
+vine_whip = move("vine_whip", "grass", 100, 45, True,False)
+razor_leaf = move("razor_leaf", "grass", 95, 55, True,False)
+magical_leaf = move("magical_leaf", "grass", 100, 60, False,False)
+giga_drain = move("giga_drain", "grass", 100, 75, False,False)
+seed_bomb = move("seed_bomb", "grass", 100, 80, True,False)
+energy_ball = move("energy_ball", "grass", 100, 90, False,False)
+leaf_blade = move("leaf_blade", "grass", 100, 90, True,False)
+solar_beam = move("solar_beam", "grass", 100, 120, False,False)
+slee_powder=move("sleep_powder","grass",75,0,False,True)
 
 # --- ELECTRIC TYPE (10) ---
-thunder_shock = move("thunder_shock", "electric", 100, 40, False)
-spark = move("spark", "electric", 100, 65, True)
-thunder_fang = move("thunder_fang", "electric", 95, 65, True)
-shock_wave = move("shock_wave", "electric", 100, 60, False)
-thunder_punch = move("thunder_punch", "electric", 100, 75, True)
-discharge = move("discharge", "electric", 100, 80, False)
-thunderbolt = move("thunderbolt", "electric", 100, 90, False)
-wild_charge = move("wild_charge", "electric", 100, 90, True)
-zap_cannon = move("zap_cannon", "electric", 50, 120, False)
-thunder = move("thunder", "electric", 70, 110, False)
+thunder_shock = move("thunder_shock", "electric", 100, 40, False,False)
+spark = move("spark", "electric", 100, 65, True,False)
+thunder_fang = move("thunder_fang", "electric", 95, 65, True,False)
+shock_wave = move("shock_wave", "electric", 100, 60, False,False)
+thunder_punch = move("thunder_punch", "electric", 100, 75, True,False)
+discharge = move("discharge", "electric", 100, 80, False,False)
+thunderbolt = move("thunderbolt", "electric", 100, 90, False,False)
+wild_charge = move("wild_charge", "electric", 100, 90, True,False)
+zap_cannon = move("zap_cannon", "electric", 50, 120, False,False)
+thunder = move("thunder", "electric", 70, 110, False,False)
 
 
 charizard_learnset = {
@@ -94,6 +95,7 @@ venusaur_learnset = {
     "solar beam": solar_beam,
     "body slam": body_slam,
     "double edge": double_edge,
+    "sleep powder": sleep_powder
     "admin":admin}
 
 blastoise_learnset = {
